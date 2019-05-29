26d1cdd8f0528202d7198322f8269e55756caa9f
root <root@pop-os.localdomain>
2019-05-28 20:35:18 -0500

    added author controller

---
+++ api/Controllers/AuthorsController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.dbl.repo;
+using api.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class AuthorsController : ControllerBase
+    {
+        private AuthorRepo authorRepo;
+        private ILogger<AuthorsController> _log;
+
+        public AuthorsController(IConfiguration config, ILogger<AuthorsController> log)
+        {
+            authorRepo = new AuthorRepo(config);
+            _log = log;
+        }
+        [HttpGet("~/api/getall/author/")]
+        public IActionResult GetAuthors()
+        {
+            _log.LogInformation(authorRepo.getConnectionString());
+            return Ok(authorRepo.FindAll());
+        }
+        [HttpGet("~/api/author/{id}")]
+        public IActionResult GetAuthors(int id)
+        {
+            return Ok(authorRepo.FindByID(id));
+        }
+
+        [HttpPost("~/api/author/")]
+        public void AddAuthor ([FromBody]Author data)
+        {
+            authorRepo.Add(data);
+            _log.LogInformation($"The author that is added {JsonConvert.SerializeObject(data)}");
+            
+        }
+
+        [HttpPut("~/api/author/{id}")]
+        public void Put(int id, [FromBody]Author data)
+        {
+
+            authorRepo.Update(data);
+            _log.LogInformation($"The author that was updated {JsonConvert.SerializeObject(data)}");
+            
+        }
+        [HttpDelete("~/api/author/{id}")]
+        public void Delete(int id)
+        {
+            authorRepo.Remove(id);
+            _log.LogInformation($"The author that was deleted {id.ToString()}");
+            
+        }
+    }
+}
